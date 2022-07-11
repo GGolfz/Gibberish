5a0b17eaad4737954d8b086ecbd188276301e084
GGolfz <wisarutgolf11@gmail.com>
2022-07-11 22:07:43 +0700

    add: csv to json :sparkles:

---
+++ modify_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import json
+
+df = pd.read_csv("gibberish.csv")
+
+data = []
+for i in df.itertuples():
+    data.append({
+        "original": i[1],
+        "gibberish": i[2]
+    })
+
+json.dump(data, open("giberrish/src/data/word_list.json", "w"))
